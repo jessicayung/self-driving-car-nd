68d882233debe8a36743ea87cb64146ccf19a7ea
Jessica Yung <jy357@cam.ac.uk>
2017-01-13 22:01:22 +0000

    feat(p3): update drive.py and model.json

--- p3-behavioural-cloning/drive-archive.py
+++ p3-behavioural-cloning/drive-archive.py
@@ -15,10 +15,6 @@ from io import BytesIO
 from keras.models import model_from_json
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array
 
-# Fix error with Keras and TensorFlow
-import tensorflow as tf
-tf.python.control_flow_ops = tf
-
 
 sio = socketio.Server()
 app = Flask(__name__)
@@ -65,14 +61,9 @@ if __name__ == '__main__':
     help='Path to model definition json. Model weights should be on the same path.')
     args = parser.parse_args()
     with open(args.model, 'r') as jfile:
-        # NOTE: if you saved the file by calling json.dump(model.to_json(), ...)
-        # then you will have to call:
-        #
-        #   model = model_from_json(json.loads(jfile.read()))\
-        #
-        # instead.
-        model = model_from_json(jfile.read())
-
+        loaded_jfile = json.load(jfile)
+        print("Loaded jfile")
+        model = model_from_json(str(loaded_jfile))
 
     model.compile("adam", "mse")
     weights_file = args.model.replace('json', 'h5')
