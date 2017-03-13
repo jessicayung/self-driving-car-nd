d47e53aff0e49aa43fa633a8ede21cefdf166968
Jessica Yung <jy357@cam.ac.uk>
2017-03-13 19:56:53 +0000

    feat(t2-p1): add kf predict, kf update functions

--- term-2/p1-extended-kalman-filter/src/kalman_filter.cpp
+++ term-2/p1-extended-kalman-filter/src/kalman_filter.cpp
@@ -22,6 +22,10 @@ void KalmanFilter::Predict() {
   TODO:
     * predict the state
   */
+    x_ = F_ * x_;
+    MatrixXd Ft = F_.transpose();
+    P_ = F_ * P_ * Ft + Q_;
+    
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
@@ -29,6 +33,19 @@ void KalmanFilter::Update(const VectorXd &z) {
   TODO:
     * update the state by using Kalman Filter equations
   */
+    VectorXd z_pred = H_ * x_;
+    VectorXd y = z - z_pred;
+    MatrixXd Ht = H_.transpose();
+    MatrixXd S = H_ * P_ * Ht + R_;
+    MatrixXd Si = S.inverse();
+    MatrixXd PHt = P_ * Ht;
+    MatrixXd K = PHt * Si;
+    
+    //new estimate
+    x_ = x_ + (K * y);
+    long x_size = x_.size();
+    MatrixXd I = MatrixXd::Identity(x_size, x_size);
+    P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
@@ -36,4 +53,5 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+    
 }
