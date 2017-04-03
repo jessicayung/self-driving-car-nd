19744f2d41304f3faf6ae6ce6b851a4bacd6eba7
Jessica Yung <jy357@cam.ac.uk>
2017-04-03 16:23:41 +0100

    feat(t2-p2): add fn to calc rmse

--- term-2/p2-unscented-kalman-filter/src/tools.cpp
+++ term-2/p2-unscented-kalman-filter/src/tools.cpp
@@ -15,4 +15,34 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+	VectorXd rmse(4);
+    rmse << 0,0,0,0;
+    
+    // check the validity of the following inputs:
+    //  * the estimation vector size should not be zero
+    //  * the estimation vector size should equal ground truth vector size
+    if(estimations.size() != ground_truth.size()
+       || estimations.size() == 0){
+        std::cout << "Invalid estimation or ground_truth data" << std::endl;
+        return rmse;
+    }
+    
+    //accumulate squared residuals
+    for(unsigned int i=0; i < estimations.size(); ++i){
+        
+        VectorXd residual = estimations[i] - ground_truth[i];
+        
+        //coefficient-wise multiplication
+        residual = residual.array()*residual.array();
+        rmse += residual;
+    }
+    
+    //calculate the mean
+    rmse = rmse/estimations.size();
+    
+    //calculate the squared root
+    rmse = rmse.array().sqrt();
+    
+    //return the result
+    return rmse;
 }
