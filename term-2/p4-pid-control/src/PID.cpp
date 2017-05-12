39e923cbc9fa6459bcd138995ccd5628d2e69d1b
Jessica Yung <jy357@cam.ac.uk>
2017-05-12 13:20:28 +0100

    feat(t2-p4): init, add untested pid control alg - PID::Init - PID::UpdateError (twiddle) - PID::TotalError (sum P, I, D errors) - Initialise pid and calc steering value using pid in main.cpp

---
+++ term-2/p4-pid-control/src/PID.cpp
@@ -0,0 +1,40 @@
+#include "PID.h"
+
+using namespace std;
+
+/*
+* TODO: Complete the PID class.
+*/
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp, double Ki, double Kd) {
+    // P: steer in proportion to the crosstrack error
+    
+    // I: steer more when there is sustained error to counter the systematic bias we have from e.g. misaligned wheels.
+    
+    // D: When the car has turned enough to reduce CTE, it counter-steers
+    // to avoid overshooting
+    
+    // Twiddle: choose optimal parameters
+    this->Kp = Kp;
+    this->Ki = Ki;
+    this->Kd = Kd;
+    sum_cte = 0;
+    prev_cte = 0;
+}
+
+void PID::UpdateError(double cte) {
+    sum_cte += cte;
+    p_error = - Kp * cte;
+    i_error = - Kd * (cte - prev_cte);
+    d_error = - Ki * sum_cte;
+    prev_cte = cte;
+}
+
+double PID::TotalError() {
+  return p_error + i_error + d_error;
+}
+
