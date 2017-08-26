600888e6e9b08c65e768a7dd8fb429790e6ab87e
Jessica Yung <jy357@cam.ac.uk>
2017-08-26 12:29:21 +0100

    feat(t3-p2): complete FCN layers fn

--- term-3/2-semantic-segmentation/main.py
+++ term-3/2-semantic-segmentation/main.py
@@ -59,7 +59,41 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :return: The Tensor for the last layer of output
     """
     # TODO: Implement function
-    return None
+
+    # 1. Pre-trained network (VGG) as encoder
+
+    # 2. 1x1 convolutions
+
+    def conv_1x1(x, num_outputs):
+        kernel_size = 1
+        stride = 1
+        return tf.layers.conv2d(x, num_outputs, kernel_size, stride)
+
+    # TODO: unsure about num_outputs=num_classes
+    conv_out = conv_1x1(vgg_layer7_out, num_classes)
+
+
+    # 3. Transposed convolutions
+    # TODO: tune kernel, stride, weight initialisations, regularisation
+    # Up-sample
+    deconv_1 = tf.layers.conv2d_transpose(conv_out, num_classes, 2,2)
+
+    # Add skip connection to previous VGG layer
+    skip_layer_1 = conv_1x1(vgg_layer4_out, num_classes)
+    skip_conn_1 = tf.add(deconv_1, skip_layer_1)
+
+    # Up-sample
+    deconv_2 = tf.layers.conv2d_transpose(skip_conn_1, num_classes, 4, 4)
+
+    # Add skip connection to previous VGG layer
+    skip_layer_2 = conv_1x1(vgg_layer3_out, num_classes)
+    skip_conn_2 = tf.add(deconv_2, skip_layer_2)
+
+    # Up-sample
+    deconv_3 = tf.layers.conv2d_transpose(skip_conn_2, num_classes, 8, 8)
+
+    return deconv_3
+
 tests.test_layers(layers)
 
 
